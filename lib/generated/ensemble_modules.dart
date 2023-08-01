f64bc8c73824a9e32cfd2c338e38eadbbc3a254b
Vu Le <vu@ensembleui.com>
2023-07-31 18:09:55 -0700

    update path

--- lib/generated/ensemble_modules.dart
+++ lib/generated/ensemble_modules.dart
@@ -4,7 +4,7 @@ import 'package:ensemble/module/auth_module.dart';
 import 'package:get_it/get_it.dart';
 
 // Uncomment to enable Auth service
-// import 'package:ensemble_auth/module/auth_module.dart';
+// import 'package:ensemble_auth/auth_module.dart';
 
 // Uncomment to enable camera services
 // import 'package:ensemble_camera/camera_manager.dart';
