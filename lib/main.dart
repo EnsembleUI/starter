23908c043a25fff050f606d3bc9f52ee57d9e6c4
snehmehta <snehmehta933@gmail.com>
2024-01-08 13:24:08 +0530

    external flutter configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'dart:ui';
 
+import 'package:ensemble/ensemble.dart';
 import 'package:ensemble/ensemble_app.dart';
 import 'package:ensemble/framework/error_handling.dart';
 import 'package:ensemble/framework/widget/error_screen.dart';
 import 'package:ensemble_starter/generated/ensemble_modules.dart';
 import 'package:flutter/material.dart';
 
-
 /// this demonstrates an App running exclusively with Ensemble
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   initErrorHandler();
   EnsembleModules().init();
-  runApp(EnsembleApp());
+  Ensemble().setExternalScreenWidgets({
+    'counterScreen': (context, args) {
+      final message = args?['message'];
+      return CounterWidget(message: message);
+    },
+  });
+  runApp(const EnsembleApp());
 }
 
 void initErrorHandler() {
@@ -34,4 +40,60 @@ void initErrorHandler() {
     debugPrint("Async Error: " + error.toString());
     return true;
   };
+}
+
+class CounterWidget extends StatefulWidget {
+  const CounterWidget({
+    super.key,
+    this.message,
+  });
+
+  final String? message;
+
+  @override
+  State<CounterWidget> createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Flutter Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              widget.message ?? 'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
